b0266fd617551e1c3699185a50abb5adfd845cfb
rapidevelop <i.vafadarnezhad@gmail.com>
2021-03-09 18:29:50 +0330

    chore: install prettier and integrate it with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "no-console": 1,
+    'no-console': 1,
   },
 };
